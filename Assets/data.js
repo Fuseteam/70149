2cf5fcc091e2bd97ebc831f6fb2f91cb23180e1b
Fuseteam <Inouchi@outlook.com>
2017-11-24 19:21:05 -0300

    Add files via upload

---
+++ Assets/data.js
@@ -0,0 +1,152 @@
+var series = {
+  "current":[{
+    "WatchLink":"cheating_craft",
+    "DownloadLink":"cheating-craft.html",
+    "Name":"Cheating Craft [一课一练]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/9/81891.webp",
+    "Latest":"01-02"
+  },
+  {
+    "WatchLink":"chinese_mystery_man",
+    "DownloadLink":"chinese_mystery_man.html",
+    "Name":"Chinese Mystery Man [中国惊奇先生]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/6/80096.webp",
+    "Latest":"29"
+  },
+  {
+    "WatchLink":"nuwa_chengzhang_riji",
+    "DownloadLink":"nuwa-chengzhang-riji.html",
+    "Name":"Nüwa Chengzhang Riji [女娲成长日记]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/7/79259.webp",
+    "Latest":"11"
+  },
+  {
+    "WatchLink":"quanzhi_fashi",
+    "DownloadLink":"quanzhi-fashi.html",
+    "Name":"Quanzhi Fashi [全职法师]",
+    "ImgUrl":"http://photocdn.sohu.com/kis/fengmian/1207/1207436/1207436_ver_big_20160906145003.jpg",
+    "Latest":"06"
+  },
+  {
+    "WatchLink":"i_am_bai_xiaofei",
+    "Download":"i-am-bai-xiaofei.html",
+    "Name":"I am Bai Xiaofei[我叫白小飞]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/5/80257.webp",
+    "Latest":"02"
+  },
+  {
+    "WatchLink":"taking_everybodys_husband_home",
+    "DownloadLink":"taking-everybodys-husband-home.html",
+    "Name":"Taking Everybody's Husband Home [国民老公带回家]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/3/83461.webp",
+    "Latest":"05"
+  },
+  {
+    "WatchLink":"the_prisoners_of_time",
+    "DownloadLink":"the-prisoners-of-time.html",
+    "Name":"The Prisoners of Time [时光诡域]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/2/81402.webp",
+    "Latest":"05"
+  },
+  {
+    "WatchLink":"twin_spirit_detectives",
+    "DownloadLink":"twin-spirit-detectives.html",
+    "Name":"Twin Spirit Detectives [双生灵探]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/4/81754.webp",
+    "Latest":"17"
+  },
+  {
+    "WatchLink":"wangpai_yushi",
+    "DownloadLink":"wangpai-yushi.html",
+    "Name":"Wangpai Yushi [王牌御史]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/13/82852.webp",
+    "Latest":"11"
+  },
+  {
+    "WatchLink":"yi_ren_zhi_xia",
+    "DownloadLink":"yi-ren-zhi-xia.html",
+    "Name":"Yi Ren Zhi Xia [一人之下]",
+    "ImgUrl":"https://myanimelist.cdn-dena.com/images/anime/12/80346.webp",
+    "Latest":"04"
+  }],
+  "onhold":[{
+    "WatchLink":"100000_Bad_Jokes",
+    "DownloadLink":"100000-bad-jokes.html",
+    "Name":"100,000 Bad Jokes [十萬個冷笑話]"
+  },
+  {
+    "WatchLink":"black_and_white_pair",
+    "DownloadLink":"black-and-white-pair.html",
+    "Name":"Black and White Pair [黑白无双]"
+  },
+  {
+    "WatchLink":"feng_wei_ying_xiong_beyond_the_worlds",
+    "DownloadLink":"feng-wei-ying-xiong-beyond-the-worlds.html",
+    "Name":"Feng Wei Ying Xiong: Beyond the Worlds [疯味英雄]"
+  },
+  {
+    "WatchLink":"ying_xiong_bie_nao_heroes_dont_joke",
+    "DownloadLink":"ying-xiong-bie-nao-heroes-dont-joke.html",
+    "Name":"Ying Xiong Bie Nao: Heroes Don't Joke [英雄别闹]"
+  }],
+  "completed":[{
+    "WatchLink":"bloodivores",
+    "DownloadLink":"bloodivores.html",
+    "Name":"Bloodivores [时空囚徒]"
+  },
+  {
+    "WatchLink":"cupids_chocolates",
+    "DownloadLink":"cupids-chocolates.html",
+    "Name":"Cupid's Chocolates [爱神巧克力ING...]"
+  },
+  {
+    "WatchLink":"die_now",
+    "DownloadLink":"die-now.html",
+    "Name":"Die Now [端脑]"
+  },
+  {
+    "WatchLink":"fox_spirit_matchmaker",
+    "DownloadLink":"fox-spirit-matchmaker.html",
+    "Name":"Fox Spirit Matchmaker [狐妖小红娘]"
+  },
+  {
+    "WatchLink":"fox_spirit_matchmaker",
+    "DownloadLink":"fox-spirit-matchmaker.html",
+    "Name":"Fox Spirit Matchmaker: Wangquan Fugui [狐妖小红娘 王权富贵]"
+  },
+  {
+    "WatchLink":"fox_spirit_matchmaker",
+    "DownloadLink":"fox-spirit-matchmaker.html",
+    "Name":"Fox Spirit Matchmaker: Yue Hong [狐妖小红娘 月红]"
+  },
+  {
+    "WatchLink":"order_designer",
+    "DownloadLink":"order-designer.html",
+    "Name":"Order Designer [死灵编码]"
+  },
+  {
+    "WatchLink":"rage_comics_the_ghost_story",
+    "DownloadLink":"rage-comics-the-ghost-story.html",
+    "Name":"Rage Comics: The Ghost Story S1 [暴走恐怖故事]"
+  },
+  {
+    "WatchLink":"soul_contract",
+    "DownloadLink":"soul-contract.html",
+    "Name":"Soul Contract [灵契]"
+  },
+  {
+    "WatchLink":"shi_xiong",
+    "DownloadLink":"shi-xiong.html",
+    "Name":"Shi Xiong [尸兄]"
+  },
+  {
+    "WatchLink":"the_devil_ring",
+    "DownloadLink":"the-devil-ring.html",
+    "Name":"The Devil Ring [戒魔人]"
+  },
+  {
+    "WatchLink":"yaoguai_mingdan",
+    "DownloadLink":"yaoguai_mingdan.html",
+    "Name":"Yaoguai Mingdan [妖怪名单]"
+  },
+]};
